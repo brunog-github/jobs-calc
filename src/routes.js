b0e0bd7dbbf18f00d92ad505679a171254989f77
Bruno <bruno.g.p897@gmail.com>
2021-08-19 16:17:33 -0300

    interaçao + calculos

--- src/routes.js
+++ src/routes.js
@@ -11,10 +11,74 @@ const profile = {
     "days-per-week": 5,
     "hours-per-day": 6,
     "vacation-per-year": 4,
+    "value-hour": 100,
 }
 
-routes.get('/', (req, res) => res.render(views + "index"))
+const jobs = [
+    {
+        id: 1,
+        name: "Pizzaria Recanto do Sabor",
+        "daily-hours": 2,
+        "total-hours": 2,
+        create_at: Date.now()
+    },
+    
+    {
+        id: 2,
+        name: "Pizzaria Mais Sabor",
+        "daily-hours": 6,
+        "total-hours": 90,
+        create_at: Date.now()
+    }
+]
+
+function remainingDays(job) {
+        const remainingDays = (job['total-hours'] / job['daily-hours']).toFixed()
+
+        const createdDate = new Date(job.create_at)
+        const dueDay = createdDate.getDate() + Number(remainingDays)
+        const dueDate = createdDate.setDate(dueDay)
+
+        const timeDiffInMs = dueDate - Date.now()
+
+        const dayInMs = 86400000
+        const dayDiff = Math.floor((timeDiffInMs / dayInMs))
+
+    return dayDiff
+}
+
+routes.get('/', (req, res) => {
+    const updateJobs = jobs.map((job) => {
+        
+        const remaining = remainingDays(job)
+        const status = remaining <= 0 ? "done" : "progress"
+
+        return {
+            ...job,
+            remaining,
+            status,
+            budget: profile['value-hour'] * job['total-hours']
+        }
+    })
+    
+    return res.render(views + "index", { jobs: updateJobs })
+})
+
 routes.get('/job', (req, res) => res.render(views + "job"))
+routes.post('/job', (req, res) => {
+    const lastID = jobs[jobs.length - 1]?.id || 1
+
+
+    jobs.push({
+        id: lastID + 1,
+        name: req.body.name,
+        "daily-hours": req.body["daily-hours"],
+        "total-hours": req.body["total-hours"],
+        create_at: Date.now()
+    })
+
+    return res.redirect('/')
+})
 routes.get('/job/edit', (req, res) => res.render(views + "job-edit"))
 routes.get('/profile', (req, res) => res.render(views + "profile", { profile }))
 
--- src/server.js
+++ src/server.js
@@ -4,11 +4,18 @@ const routes = require('./routes')
 
 //cria o servidor e a porta
 const server = express()
-const port = 3000;
+const port = 3000
 
+//usando template engine
 server.set('view engine', 'ejs')
 
+//habilita arquivos statics
 server.use(express.static('public'))
+
+//usar o req.body
+server.use(express.urlencoded({ extended: true }))
+
+//routes
 server.use(routes)
 
-server.listen(port, () => { console.log('Rodando...') })
+server.listen(port, () => { console.log('Rodando..') })
