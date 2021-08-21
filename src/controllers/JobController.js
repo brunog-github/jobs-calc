93c5560fe616ba0335ccd335d2870f7343c10bf5
Bruno <bruno.g.p897@gmail.com>
2021-08-21 15:55:05 -0300

    bancos de dados SQlite

--- src/controllers/JobController.js
+++ src/controllers/JobController.js
@@ -10,14 +10,14 @@ module.exports = {
     save(req, res) {
         const lastID = Job.get()[Job.get().length - 1]?.id || 0
     
-        Job.get().push({
+        Job.create({
             id: lastID + 1,
             name: req.body.name,
             "daily-hours": req.body["daily-hours"],
             "total-hours": req.body["total-hours"],
             create_at: Date.now()
         })
-    
+        
         return res.redirect('/')
     },
 
